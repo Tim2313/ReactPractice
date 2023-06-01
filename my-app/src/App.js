1c4ecdfb73475818c0d50c003afc7ebbbbc1e1ec
Tim2313 <tkisuk@gmail.com>
2023-06-01 21:29:15 +0300

    [ReactPractice] init

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div>This is my ReactApp</div>
+      <div className="App">
+        <ul>
+          <li>Java</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
